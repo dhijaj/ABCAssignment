21af0b512eed400b818a018cfee56bc72185f372
dhijaj <dhineshprabu@ymail.com>
2017-07-04 22:28:44 +1000

    Section B TCs added

--- ATU Reports/Results/barChart.js
+++ ATU Reports/Results/barChart.js
@@ -1,9 +1,9 @@
             $(document).ready(function(){
-                var s1 = [5,1,1,0,0,0,0,1,3,8,8];
-                var s2 = [0,0,0,1,0,1,1,0,0,0,2];
-                var s3 = [0,0,0,0,1,0,0,0,0,0,0];
+                var s1 = [3,3,3,3,3,3,3,3,3,11,11];
+                var s2 = [3,3,3,3,3,3,3,3,3,2,0];
+                var s3 = [0,0,0,0,0,0,0,0,0,0,0];
 
-var ticks = [69,70,71,72,73,74,75,76,77,78];    $.jqplot('bar', [s1, s2, s3], {
+var ticks = [113,114,115,116,117,118,119,120,121,122];    $.jqplot('bar', [s1, s2, s3], {
         animate: true,axesDefaults:{min:0,tickInterval: 1},        seriesColors: ["#7BB661", "#E03C31", "#21ABCD"],
         stackSeries: false,
         seriesDefaults: {
